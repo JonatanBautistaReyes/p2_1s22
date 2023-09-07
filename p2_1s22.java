a12e3452325540e74635d48dea13430007d296e1
JonatanBautistaReyes <144191150+JonatanBautistaReyes@users.noreply.github.com>
2023-09-06 18:56:30 -0600

    Add files via upload

---
+++ p2_1s22.java
@@ -0,0 +1,9 @@
+class p2_1s22 {
+	public static void main(String[] args) {
+		System.out.println("Jonatan Bautista Reyes");
+		System.out.println("18 años");
+		System.out.println("jonatanbautista505@gmail.com");
+		System.out.println("futbol");
+		System.out.println("azul");
+	}
+}
